75ce6e2415e831769e9e5f1b62106e72a6298d81
noooey <20203065@kookmin.ac.kr>
2023-06-13 11:41:39 +0900

    :zap: reservoir

---
+++ reservoir.py
@@ -0,0 +1,23 @@
+import random
+
+class Reservoir:
+    def __init__(self, k):
+        self.k = k
+        self.sampled = []
+        self.size = 0
+
+    def add(self, x):
+        self.size += 1
+
+        if self.size <= self.k:
+            self.sampled.append(x)
+        else:
+            i = random.randrange(0, self.size)
+            if i < self.k:
+                self.sampled[i] = x
+
+r = Reservoir(100)
+
+for i in range(10000):
+    r.add(i)
+    print(i, r.sampled)
