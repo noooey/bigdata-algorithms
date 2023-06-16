aab478ec21c7db3cf380159b4b93e57785e77224
noooey <20203065@kookmin.ac.kr>
2023-06-16 16:09:48 +0900

    :zap: test

--- reservoir_with_replacement.py
+++ reservoir_with_replacement.py
@@ -1,8 +1,3 @@
-"""
-국민대학교 소프트웨어학부
-23-1학기 빅데이터최신기술 실습 코드
-"""
-
 import random
 import matplotlib.pyplot as plt
 
@@ -18,9 +13,8 @@ class Reservoir:
         if self.size <= self.k:
             self.sampled.append(x)
         else:
-            i = random.randrange(0, self.size)
-            if i < self.k:
-                self.sampled[i] = x
+            i = random.randrange(0, self.k)
+            self.sampled[i] = x
 
 # 시행 횟수
 num_iterations = 10000
